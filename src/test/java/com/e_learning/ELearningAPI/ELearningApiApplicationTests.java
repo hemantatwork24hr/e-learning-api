9af27cabc6e0323e4f3789306bad5130e141b439
Hemant Gajbe <hemantatwork24hr@gmail.com>
2024-07-26 08:19:59 +0530

    Initial setup

---
+++ src/test/java/com/e_learning/ELearningAPI/ELearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_learning.ELearningAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
